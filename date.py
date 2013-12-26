1ef670c721a489d0cf09c43a371f6cfef47c1c2f
DanMcInerney <danhmcinerney@gmail.com>
2013-12-26 01:18:33 -0500

    bug fixes and notes

--- date.py
+++ date.py
@@ -1,13 +1,19 @@
 print "Welcome to the online date picker! Let's start with your name. So, what is it?"
 name = raw_input()
-print "aah, so your name is %s. Mine too!! jk." % (name)
+print "aah, so your name is %s. Mine too!! jk." % name
 age = raw_input("How old are you? ")
-if age > 40:
+if int(age) > 40:
+'''
+Note that raw_input is always going to result in a string so to turn a string
+of numbers into an integer object which can then have the greater than or
+less than sign applied to it, you must use int(string). To see what kind of object
+a variable is, you can use type(variable).
+'''
     print "You are too old to be dating. Get a life."
 else:
-    print "Great! so are you a male or female? just type m or f."
-    if raw.input() == "f":
+    mf = raw_input("Great! so are you a male or female? just type m or f.")
+    if mf == "f":
         print  "Sorry, this is a male-only dating service."
     else:
         print "Sorry, this is a female-only dating service."
-    
+
