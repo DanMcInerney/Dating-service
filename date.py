8e761dc8a529f946acca98809662ae0530ce3c12
DanMcInerney <danhmcinerney@gmail.com>
2013-12-26 01:38:40 -0500

    Did not test last code, forgot to match indentation

--- date.py
+++ date.py
@@ -3,15 +3,10 @@ name = raw_input()
 print "aah, so your name is %s. Mine too!! jk." % name
 age = raw_input("How old are you? ")
 if int(age) > 40:
-'''
-Note that raw_input is always going to result in a string so to turn a string
-of numbers into an integer object which can then have the greater than or
-less than sign applied to it, you must use int(string). To see what kind of object
-a variable is, you can use type(variable).
-'''
+    '''Note that raw_input is always going to result in a string so to turn a string of numbers into an integer object which can then have the greater than or less than sign applied to it, you must use int(string). To see what kind of object a variable is, you can use type(variable).'''
     print "You are too old to be dating. Get a life."
 else:
-    mf = raw_input("Great! so are you a male or female? just type m or f.")
+    mf = raw_input("Great! so are you a male or female? [m/f] ")
     if mf == "f":
         print  "Sorry, this is a male-only dating service."
     else:
