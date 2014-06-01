e32b7f101bd538a5b33916483d2f411d3ca7986e
Sean S <spsful@live.com>
2014-06-01 15:24:14 -0400

    Updated
    used parenthesis for "m/f" because it looked better

--- date.py
+++ date.py
@@ -6,7 +6,7 @@ if int(age) > 40:
     '''Note that raw_input is always going to result in a string so to turn a string of numbers into an integer object which can then have the greater than or less than sign applied to it, you must use int(string). To see what kind of object a variable is, you can use type(variable).'''
     print "You are too old to be dating. Get a life."
 else:
-    mf = raw_input("Great! so are you a male or female? [m/f] ")
+    mf = raw_input("Great! so are you a male or female? (m/f) ")
     if mf == "f":
         print  "Sorry, this is a male-only dating service."
     else:
